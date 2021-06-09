de3bd22d7e7afc4d009f2fc0dec4eafaa65dd997
HeathChang <jannhyunsoo@gmail.com>
2021-06-09 17:33:03 +0900

    210609_detailPage&reviewIDU

--- PlayGround/src/main/java/com/controller/game/DetailGameController.java
+++ PlayGround/src/main/java/com/controller/game/DetailGameController.java
@@ -1,15 +1,21 @@
 package com.controller.game;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dto.GameDTO;
+import com.dto.MemberDTO;
 import com.dto.RateDTO;
 import com.dto.ReviewDTO;
 import com.service.GameService;
@@ -29,7 +35,6 @@ public class DetailGameController {
 
 	@RequestMapping("/Game/detailPage")
 	public ModelAndView gameDetail(@RequestParam("gameNo") int gameNo) {
-		
 		ModelAndView model = new ModelAndView();
 		//1-1: 게임정보 
 		GameDTO gameDTO= gameService.detailGameSelect(gameNo);
@@ -55,4 +60,47 @@ public class DetailGameController {
 		model.setViewName("../detailPage");
 		return model;
 	}
+	
+	
+	@RequestMapping("/loginCheck/reviewInsert")
+	public String cartAdd(@ModelAttribute ReviewDTO rDTO, HttpSession session) {
+		System.out.println("댓글 입력값 확인: "+ rDTO);
+		int reviewResult=0;
+		String rName=rDTO.getMbrName();	
+		int gameNo=Integer.parseInt(rDTO.getGameNo());
+		System.out.println("넘어온 정보: " + rName + "\t" + gameNo);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("mbrname", rName);
+		map.put("gameNo", gameNo);
+		
+		int nameCheck = reviewService.nameCheck(map);
+		if(nameCheck==0) {
+			reviewResult = reviewService.reviewInsert(rDTO);
+			System.out.println("댓글삽입 성공: " + reviewResult);
+		}else {
+			System.out.println("댓글삽입 실패: " + reviewResult);
+
+		}
+		return "detailPage?gameNo="+rDTO.getGameNo();
+	}
+	
+	@RequestMapping(value = "/loginCheck/reviewDelete")
+	@ResponseBody
+	public void cartDelte(@RequestParam("reviewId") int reviewId) {
+		System.out.println("삭제할 reviewId: "+reviewId);
+		int result = reviewService.reviewDelete(reviewId);
+		System.out.println("댓글 삭제 성공 여부: "+result);
+		
+
+	}
+	
+	@RequestMapping(value = "/loginCheck/reviewUpdate")
+	@ResponseBody
+	public void cartUpdate(@ModelAttribute ReviewDTO rDTO) {
+		System.out.println("댓글 수정값 확인: "+rDTO.toString());
+		int result = reviewService.reviewUpdate(rDTO);
+		System.out.println("댓글 수정 성공 여부: "+result);
+	}
+	
+	
 }
